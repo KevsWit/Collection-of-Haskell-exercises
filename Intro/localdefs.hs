e43d1d3b26467060201cc4ece15a81bffcb271d9
Kevin Castillo <96481791+KevsWit@users.noreply.github.com>
2025-03-11 19:36:54 -0500

    Correct localdefs
    Correct localdefs

--- Intro/localdefs.hs
+++ Intro/localdefs.hs
@@ -5,7 +5,7 @@ fastExp :: Integer -> Integer -> Integer
 fastExp _ 0 = 1
 fastExp x n
     | even n    = y * y
-    | otherwise = y * y * n
+    | otherwise = y * y * x
     where
         y = fastExp x n_halved
         n_halved = div n 2
